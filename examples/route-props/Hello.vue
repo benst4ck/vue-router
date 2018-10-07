fbd520da3235a2b49572306628831249dbfe2f96
benst4ck <benst4ck@gmail.com>
2018-10-07 12:58:36 +0800

    null

--- examples/route-props/Hello.vue
+++ examples/route-props/Hello.vue
@@ -14,4 +14,6 @@ export default {
     }
   }
 }
+// 这里通过props指定的name指明期待父组件传递的name为String类型 如果父组件没有传递name属性那么默认值为"Vue!"
+// 在template中 name来自于父组件传递的值 $attrs包含了父作用域(<router-view>)中除了class和style以外绑定的属性
 </script>
--- examples/route-props/app.js
+++ examples/route-props/app.js
@@ -5,10 +5,10 @@ import Hello from './Hello.vue'
 Vue.use(VueRouter)
 
 function dynamicPropsFn (route) {
-  console.log(route)
+  console.log(route)  // 打印访问当前路径所匹配的路由
   const now = new Date()
-  return {
-    name: (now.getFullYear() + parseInt(route.params.years)) + '!'
+  return {  // 该函数返回一个包含name属性的对象 通过route.params.years获取动态片段years的值
+    name: (now.getFullYear() + parseInt(route.params.years)) + '!'  // 数字加字符串 结果为字符串
   }
 }
 
@@ -16,10 +16,11 @@ const router = new VueRouter({
   mode: 'history',
   base: __dirname,
   routes: [
-    { path: '/', component: Hello }, // No props, no nothing
-    { path: '/hello/:name', component: Hello, props: true }, // Pass route.params to props
-    { path: '/static', component: Hello, props: { name: 'world' }}, // static values
-    { path: '/dynamic/:years', component: Hello, props: dynamicPropsFn }, // custom logic for mapping between route and props
+    { path: '/', component: Hello },
+    // 通过props向子组件传递数据 当然 前提是需要在子组件中事先声明期望接收什么样的数据
+    { path: '/hello/:name', component: Hello, props: true }, // 传递route.params到props 它包含了动态片段和全匹配片段 这时候动态片段name拿到的值会传递到Hello组件中
+    { path: '/static', component: Hello, props: { name: 'world' }}, // 传递静态值到Hello组件中
+    { path: '/dynamic/:years', component: Hello, props: dynamicPropsFn },  // 访问该路径时匹配到的路由会作为参数传递到dynamicPropsFn函数中
     { path: '/attrs', component: Hello, props: { name: 'attrs' }}
   ]
 })
